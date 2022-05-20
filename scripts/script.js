b4372a7b8eda99327191f0eedc97c94df48d9746
adammarostica <10792897+adammarostica@users.noreply.github.com>
2022-05-20 16:04:24 -0400

    Corrected some syntax errors made by editing in the last pull request

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ cocktailApp.init = () => {
 
 cocktailApp.form = document.querySelector('form');
 cocktailApp.drinkList = document.querySelector('.drink__list');
+cocktailApp.userInput = document.getElementById('search-bar');
 
 // Get user's input and fetch the data from API
 cocktailApp.getIngredientName = function() {
@@ -13,7 +14,6 @@ cocktailApp.getIngredientName = function() {
   cocktailApp.form.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    cocktailApp.userInput = document.getElementById('search-bar');
     // Just left this in here for now
     // when fxn inputUrl is declared, we can delete this and change the fetch structure
     const ingredientType = cocktailApp.userInput.value;
@@ -22,10 +22,10 @@ cocktailApp.getIngredientName = function() {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientType}`)
     .then((res) => {
       console.log(res);
-      if (!res.ok) {
-        throw new Error(`This seems like invalid ingredient. Please try another one`);
-      } else {
+      if (res.ok) {
         return res.json();
+      } else {
+        throw new Error(`This seems like invalid ingredient. Please try another one`);
       }
     }).then((drinkData) => {
       cocktailApp.displayDrinks(drinkData);
