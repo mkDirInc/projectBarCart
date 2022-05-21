3bd995f24cf61debb9afc13f57fe6b0a926e2a63
adammarostica <10792897+adammarostica@users.noreply.github.com>
2022-05-20 22:24:33 -0400

    Radio buttons now change search type

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,55 @@
 const cocktailApp = {};
+cocktailApp.drinks = {};
+cocktailApp.searchTypes = {};
+cocktailApp.searchTypes.name = '/api/json/v1/1/search.php?s=';
+cocktailApp.searchTypes.ingredient = '/api/json/v1/1/filter.php?i=';
+cocktailApp.endpoint = 'https://www.thecocktaildb.com';
+cocktailApp.ingredientPath = '/api/json/v1/1/filter.php';
+cocktailApp.form = document.querySelector('form');
+cocktailApp.drinkList = document.querySelector('.drink__list');
+cocktailApp.userInput = document.getElementById('search-bar');
 
 cocktailApp.init = () => {
   cocktailApp.getIngredientName();
 };
 
-cocktailApp.form = document.querySelector('form');
-cocktailApp.drinkList = document.querySelector('.drink__list');
-cocktailApp.userInput = document.getElementById('search-bar');
+cocktailApp.getFetchAddress = function() {
+  const searchTerm = this.userInput.value;
+  const searchTypeChoice = document.querySelector('input[name="search-type"]:checked').value;
+  
+  return `${this.endpoint}${this.searchTypes[searchTypeChoice]}${searchTerm}`;
+}
 
 // Get user's input and fetch the data from API
 cocktailApp.getIngredientName = function() {
   
   cocktailApp.form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    // Just left this in here for now
-    // when fxn inputUrl is declared, we can delete this and change the fetch structure
-    const ingredientType = cocktailApp.userInput.value;
-    console.log(ingredientType);
-    // fetch(`inputUrl`)
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredientType}`)
-    .then((res) => {
-      console.log(res);
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error(`This seems like invalid ingredient. Please try another one`);
-      }
-    }).then((drinkData) => {
-      cocktailApp.displayDrinks(drinkData);
-    }).catch((err) => {
-      // If we have time, we can make an img display
-      alert(`This seems like invalid ingredient. Please try another one`);
-    })
+        
+    fetch(this.getFetchAddress())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(`This seems like invalid ingredient. Please try another one`);
+        }
+      })
+      .then((drinkData) => {
+        cocktailApp.displayDrinks(drinkData);
+      })
+      .catch((err) => {
+        // If we have time, we can make an img display
+        alert(`This seems like invalid ingredient. Please try another one`);
+      })
   })
 }
 
 cocktailApp.displayDrinks = function(data) {
   
-  const drinkResult = data.drinks;
-  
-  drinkResult.forEach((drink) => {
+  // Saving this in the cocktailApp namespace so that we can populate cardbacks with it on demand.
+  this.drinks = data.drinks;
+
+  this.drinks.forEach((drink) => {
     
     const drinkName = drink.strDrink;
     const drinkImg = drink.strDrinkThumb;
