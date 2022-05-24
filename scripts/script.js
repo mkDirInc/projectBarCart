1b7fe569072ed65d5d943a6a74cbb2b333c18c18
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2022-05-23 22:29:09 -0600

    created/added carousel result list container and its styling

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,18 @@ cocktailApp.form = document.querySelector('form');
 cocktailApp.drinkList = document.querySelector('.drink__list');
 cocktailApp.userInput = document.getElementById('search-bar');
 
+cocktailApp.nxtBtn = document.querySelector('.nxtArrow');
+cocktailApp.prevBtn = document.querySelector('.prevArrow');
+
 cocktailApp.init = () => {
   cocktailApp.getIngredientName();
 };
 
 cocktailApp.getFetchURL = function() {
   const searchTerm = this.userInput.value;
+  console.log(searchTerm);
   const searchTypeChoice = document.querySelector('input[name="search-type"]:checked').value;
-  
+  console.log(searchTypeChoice);
   return `${this.endpoint}${this.searchTypes[searchTypeChoice]}${searchTerm}`;
 }
 
@@ -70,7 +74,53 @@ cocktailApp.displayDrinks = function(data) {
     
     drinkResultList.append(drinkResultImg, drinkResultName);
     cocktailApp.drinkList.append(drinkResultList);
+    
   })
+
+  // carousel list
+    // call imgResult List in array
+  cocktailApp.slideImg = Array.from(cocktailApp.drinkList.children);
+    // adding currentPic class on first img result list
+  cocktailApp.slideImg[0].classList.add('current__pic');
+    // setting initial slideSize
+  cocktailApp.slideSize = cocktailApp.slideImg[0].clientWidth;
+    // by using forEach(), setting each img size
+  cocktailApp.slideImg.forEach((pic, i) => {
+    pic.style.left = cocktailApp.slideSize * i +'px';
+  });
+
+
+  // cocktailApp.movePic = (drinkList, currentPic, targetPic) => {
+  //   this.drinkList.style.transform = 'translateX(-' + targetPic.style.left + ')';
+  //   currentPic.classList.remove('current__pic');
+  //   targetPic.classList.add('current__pic');
+  // }
+
+  cocktailApp.nxtBtn.addEventListener('click', e => {
+    const currentPic = cocktailApp.drinkList.querySelector('.current__pic');
+    const nxtPic = currentPic.nextElementSibling;
+    const sizeToSlide = nxtPic.style.left;
+    cocktailApp.drinkList.style.transform = 'translateX(-' + sizeToSlide + ')';
+
+    currentPic.classList.remove('current__pic');
+    nxtPic.classList.add('current__pic');
+    
+    // cocktailApp.movePic(drinkList, currentPic, nxtPic);
+  });
+
+  cocktailApp.prevBtn.addEventListener('click', e => {
+    const currentPic = cocktailApp.drinkList.querySelector('.current__pic');
+    const nxtPic = currentPic.previousElementSibling;
+    const sizeToSlide = nxtPic.style.left;
+    cocktailApp.drinkList.style.transform = 'translateX(-' + sizeToSlide + ')';
+
+    currentPic.classList.remove('current__pic');
+    nxtPic.classList.add('current__pic');
+    
+    // cocktailApp.movePic(drinkList, currentPic, nxtPic);
+  });
+
+
 }
 
 
